8f8c3fe7bc5f80d7042002f7fc99c4360b79874e
CemCanplt <cem--canpolat@hotmail.com>
2024-12-28 21:18:46 +0300

    Update Register.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import {
   Button,
   Card,
   CardHeader,
   Form,
+  FormFeedback,
   FormGroup,
   Input,
   Label,
@@ -17,28 +18,105 @@ const initialState = {
   password: "",
 };
 
+const errorMessages = {
+  ad: "Adınız en az 3 karakter olmalıdır.",
+  soyad: "Soyadınız en az 3 karakter olmalıdır.",
+  email: "Lütfen geçerli bir mail girin.",
+  password: "Şifreniz en az 8 karakter olmak zorundadır.",
+};
+
+const initialErrors = {
+  ad: false,
+  soyad: false,
+  email: false,
+  password: false,
+};
+
 function Register() {
   const [formData, setFormData] = useState(initialState);
+  const [errors, setErrors] = useState(initialErrors);
+  const [isValid, setIsValid] = useState(false);
+
+  useEffect(() => {
+    const errorsValues = Object.values(errors);
+    const isTrue = errorsValues.includes(true);
+    // isTrue false ise geçireceğim
+    if (isTrue) {
+      setIsValid(false);
+    } else {
+      setIsValid(true);
+    }
+  }, [errors]);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
+
+    if (!isValid) {
+      return;
+    }
+
+    if (name == "email") {
+      if (validateEmail(value)) {
+        setErrors({ ...errors, [name]: false });
+      } else {
+        setErrors({ ...errors, [name]: true });
+      }
+    }
+
+    if (name == "password") {
+      if (validatePassword(value)) {
+        setErrors({ ...errors, [name]: false });
+      } else {
+        setErrors({ ...errors, [name]: true });
+      }
+    }
+
+    if (name == "ad") {
+      if (value.length > 3) {
+        setErrors({ ...errors, [name]: false });
+      } else {
+        setErrors({ ...errors, [name]: true });
+      }
+    }
+
+    if (name == "soyad") {
+      if (value.length > 3) {
+        setErrors({ ...errors, [name]: false });
+      } else {
+        setErrors({ ...errors, [name]: true });
+      }
+    }
   };
 
-  const handleSubmit = () => {
-    axios
-      .post("", formData)
-      .then(() => {
+  // yardımcı fonksiyon - mail kontrol
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
 
-        
-      })
+  // yardımcı fonksiyon - şifre kontrol
+  const validatePassword = (password) => {
+    return String(password).match(
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+    );
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("https://reqres.in/api/users", formData)
+      .then(() => {})
       .catch((error) => console.log("GİTMEDİ HATA", error));
   };
 
   return (
     <Card>
       <CardHeader>Kayıt Olma</CardHeader>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <FormGroup>
           <Label for="ad">Ad</Label>
           <Input
@@ -48,7 +126,9 @@ function Register() {
             type="text"
             onChange={handleChange}
             value={formData.ad}
+            invalid={errors.ad}
           />
+          {errors.ad && <FormFeedback>{errorMessages.ad}</FormFeedback>}
         </FormGroup>
         <FormGroup>
           <Label for="soyad">Soyad</Label>
@@ -59,7 +139,9 @@ function Register() {
             type="text"
             onChange={handleChange}
             value={formData.soyad}
+            invalid={errors.soyad}
           />
+          <FormFeedback>{errorMessages.soyad}</FormFeedback>
         </FormGroup>
         <FormGroup>
           <Label for="email">Email</Label>
@@ -70,7 +152,9 @@ function Register() {
             type="email"
             onChange={handleChange}
             value={formData.email}
+            invalid={errors.email}
           />
+          {errors.email && <FormFeedback>{errorMessages.email}</FormFeedback>}
         </FormGroup>
         <FormGroup>
           <Label for="password">Password</Label>
@@ -81,7 +165,9 @@ function Register() {
             type="password"
             onChange={handleChange}
             value={formData.password}
+            invalid={errors.password}
           />
+          <FormFeedback>{errorMessages.password}</FormFeedback>
         </FormGroup>
         <Button>Kayıt Ol</Button>
       </Form>
